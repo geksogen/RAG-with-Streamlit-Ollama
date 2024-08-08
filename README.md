033ae9a5dc8162938f6e369c62dbaf79efa4289b
Andrei <test@test.mail>
2024-08-08 17:10:58 +0300

    add describe to README.md

--- README.md
+++ README.md
@@ -35,8 +35,22 @@ ollama run owl/t-lite
 
 ### Run App
 ```BASH
-git clone 
+git clone https://github.com/AIDevBytes/Streamlit-Ollama-Chatbot.git
 apt install python3-pip
 pip3 install -r requirements.txt
 streamlit run main.py
 ```
+
+
+
+## Clear
+```BASH
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+docker system prune -a
+```
+
+
+## LLM models
+orca-mini:3b
+owl/t-lite
--- application/main.py
+++ application/main.py
@@ -1,26 +1,50 @@
-import os
 import streamlit as st
-from doc_chat_utulity import get_answer
-
-working_dir = os.path.dirname(os.path.abspath(__file__))
+from config import Config
+from helpers.llm_helper import chat, stream_parser
 
 st.set_page_config(
-    page_title="Chat with Doc",
-    page_icon="🦙",
-    layout="centered"
+    page_title=Config.PAGE_TITLE,
+    initial_sidebar_state="expanded"
 )
 
-st.title("Document - Llama 3 - Ollama")
+st.title(Config.PAGE_TITLE)
+st.image(
+            "https://masterpiecer-images.s3.yandex.net/a45eafee77d111eebac46a0259d7362a:upscaled",
+            width=400, # Manually Adjust the width of the image as per requirement
+        )
 
-uploaded_file = st.file_uploader(label="Uploaded your file", type=["pdf"])
-user_query = st.text_input("Your question")
+# sets up sidebar nav widgets
+with st.sidebar:
+    st.markdown("# Chat Options")
 
-if st.button("Run"):
-    bytes_data = uploaded_file.read()
-    file_name = uploaded_file.name
-    file_path = os.path.join(working_dir, file_name)
-    with open(file_path, "wb") as f:
-        f.write(bytes_data)
-    answer = get_answer(file_name, user_query)
+    # widget - https://docs.streamlit.io/library/api-reference/widgets/st.selectbox
+    model = st.selectbox('What model would you like to use?', Config.OLLAMA_MODELS)
 
-    st.success(answer)
+# checks for existing messages in session state
+# https://docs.streamlit.io/library/api-reference/session-state
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from session state
+# https://docs.streamlit.io/library/api-reference/chat/st.chat_message
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if user_prompt := st.chat_input("What would you like to ask?"):
+    # Display user prompt in chat message widget
+    with st.chat_message("user"):
+        st.markdown(user_prompt)
+
+    # adds user's prompt to session state
+    st.session_state.messages.append({"role": "user", "content": user_prompt})
+
+    with st.spinner('Generating response...'):
+        # retrieves response from model
+        llm_stream = chat(user_prompt, model=model)
+
+        # streams the response back to the screen
+        stream_output = st.write_stream(stream_parser(llm_stream))
+
+        # appends response to the message list
+        st.session_state.messages.append({"role": "assistant", "content": stream_output})
