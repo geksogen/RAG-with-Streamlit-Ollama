16fb214c5869cebc019f77e2d7d9f62075b8bc14
Andrei <test@test.mail>
2024-08-09 16:59:41 +0300

    RAG Langchain

--- application/RAG_Langchain/main.py
+++ application/RAG_Langchain/main.py
@@ -1,7 +1,7 @@
 from langchain_community.document_loaders import WebBaseLoader
 import os
 
-os.environ['USER_AGENT'] = 'Chrome/127.0.0.0 Safari/537.36'
+os.environ['USER_AGENT'] = 'user-agent: Mozilla/5.0 (iPad; U; CPU OS 3_2_1 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Mobile/7B405'
 
 
 loader = WebBaseLoader("https://ru.wikipedia.org/wiki/%D0%93%D1%80%D0%B0%D0%B2%D0%B8%D1%86%D0%B0%D0%BF%D0%BF%D0%B0")
