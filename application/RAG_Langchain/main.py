c0abfdba040a107f2730aee33c04a5809b372eb1
Andrei <test@test.mail>
2024-08-09 17:00:13 +0300

    RAG Langchain

--- application/RAG_Langchain/main.py
+++ application/RAG_Langchain/main.py
@@ -6,3 +6,5 @@ os.environ['USER_AGENT'] = 'user-agent: Mozilla/5.0 (iPad; U; CPU OS 3_2_1 like 
 
 loader = WebBaseLoader("https://ru.wikipedia.org/wiki/%D0%93%D1%80%D0%B0%D0%B2%D0%B8%D1%86%D0%B0%D0%BF%D0%BF%D0%B0")
 data = loader.load()
+
+print(data)
