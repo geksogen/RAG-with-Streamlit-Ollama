db91f27a0960f3308bb54ff8626574ad28eaf498
Andrei <test@test.mail>
2024-08-09 17:01:57 +0300

    RAG Langchain

--- application/RAG_Langchain/main.py
+++ application/RAG_Langchain/main.py
@@ -1,10 +1,14 @@
 from langchain_community.document_loaders import WebBaseLoader
-import os
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-os.environ['USER_AGENT'] = 'user-agent: Mozilla/5.0 (iPad; U; CPU OS 3_2_1 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Mobile/7B405'
-
-
+# Load data
 loader = WebBaseLoader("https://ru.wikipedia.org/wiki/%D0%93%D1%80%D0%B0%D0%B2%D0%B8%D1%86%D0%B0%D0%BF%D0%BF%D0%B0")
 data = loader.load()
 
-print(data)
+# Splitting data
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+all_splits = text_splitter.split_documents(data)
+
+print(all_splits)
+
+
