aad50254a073c1b55bbf8d1bf1c80491ac5c486f
Andrei <test@test.mail>
2024-08-09 17:15:50 +0300

    RAG Langchain

--- application/RAG_Langchain/main.py
+++ application/RAG_Langchain/main.py
@@ -2,6 +2,11 @@ from langchain_community.document_loaders import WebBaseLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.embeddings import SentenceTransformerEmbeddings
 
+from langchain_core.output_parsers import StrOutputParser
+from langchain.prompts import ChatPromptTemplate
+from langchain_community.chat_models import ChatOllama
+from langchain_core.runnables import RunnableLambda, RunnablePassthrough
+
 # Load data
 loader = WebBaseLoader("https://ru.wikipedia.org/wiki/%D0%93%D1%80%D0%B0%D0%B2%D0%B8%D1%86%D0%B0%D0%BF%D0%BF%D0%B0")
 data = loader.load()
@@ -25,6 +30,27 @@ retriever = vectorstore.as_retriever()
 question = "What are Gravicapa?"
 docs = vectorstore.similarity_search(question)
 
-print(docs[0])
+# Prompt
+template = """Answer the question based only on the following context:
+{context}
+
+Question: {question}
+"""
+prompt = ChatPromptTemplate.from_template(template)
+
+# Local LLM
+ollama_llm = "orca-mini:3b"
+model_local = ChatOllama(model=ollama_llm)
+
+# Chain
+chain = (
+    {"context": retriever, "question": RunnablePassthrough()}
+    | prompt
+    | model_local
+    | StrOutputParser()
+)
 
 
+q = chain.invoke("What are the types of agent memory?")
+
+print(q)
