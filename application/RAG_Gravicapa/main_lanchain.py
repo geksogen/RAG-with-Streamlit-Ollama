b2d420103846a4c250939be4951631ed21c978e5
Andrei <test@test.mail>
2024-08-09 17:58:59 +0300

    RAG Langchain

--- application/RAG_Gravicapa/main_lanchain.py
+++ application/RAG_Gravicapa/main_lanchain.py
@@ -3,7 +3,7 @@ import requests
 import json
 
 # This is a large nested json object and will be loaded as a python dict
-json_data = requests.get("https://ru.wikipedia.org/wiki/%D0%93%D1%80%D0%B0%D0%B2%D0%B8%D1%86%D0%B0%D0%BF%D0%BF%D0%B0").json()
+json_data = requests.get("https://api.smith.langchain.com/openapi.json").json()
 
 splitter = RecursiveJsonSplitter(max_chunk_size=300)
 json_chunks = splitter.split_json(json_data=json_data)
