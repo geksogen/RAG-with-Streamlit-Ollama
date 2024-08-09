ff188b25303da16153434e9b7b5fc227cd4d2fdd
Andrei <test@test.mail>
2024-08-09 17:57:50 +0300

    RAG Langchain

--- application/RAG_Gravicapa/main_lanchain.py
+++ application/RAG_Gravicapa/main_lanchain.py
@@ -1,55 +1,14 @@
-from langchain_community.document_loaders import WebBaseLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_text_splitters import RecursiveJsonSplitter
-import ollama
-import chromadb
-
-import json
 import requests
+import json
 
-# Load data
-loader = WebBaseLoader("https://ru.wikipedia.org/wiki/%D0%93%D1%80%D0%B0%D0%B2%D0%B8%D1%86%D0%B0%D0%BF%D0%BF%D0%B0")
-data = loader.load()
+# This is a large nested json object and will be loaded as a python dict
+json_data = requests.get("https://ru.wikipedia.org/wiki/%D0%93%D1%80%D0%B0%D0%B2%D0%B8%D1%86%D0%B0%D0%BF%D0%BF%D0%B0").json()
 
+splitter = RecursiveJsonSplitter(max_chunk_size=300)
+json_chunks = splitter.split_json(json_data=json_data)
 
-# Splitting data
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
-all_splits = text_splitter.split_documents(data)
+docs = splitter.create_documents(texts=[json_data])
 
-
-documents = json.dumps(all_splits)
-
-client = chromadb.Client()
-collection = client.create_collection(name="docs_1")
-
-# store each document in a vector embedding database
-for i, d in enumerate(documents):
-  response = ollama.embeddings(model="mxbai-embed-large", prompt=d)
-  embedding = response["embedding"]
-  collection.add(
-    ids=[str(i)],
-    embeddings=[embedding],
-    documents=[d]
-  )
-
-# an example prompt
-prompt = "What is Gravicapa?"
-
-# generate an embedding for the prompt and retrieve the most relevant doc
-response = ollama.embeddings(
-  prompt=prompt,
-  model="mxbai-embed-large"
-)
-results = collection.query(
-  query_embeddings=[response["embedding"]],
-  n_results=1
-)
-data = results['documents'][0][0]
-
-# generate a response combining the prompt and data we retrieved in step 2
-output = ollama.generate(
-  model="orca-mini:3b",
-  prompt=f"Using this data: {data}. Respond to this prompt: {prompt}"
-)
-
-print(output['response'])
+for doc in docs[:3]:
+    print(doc)
