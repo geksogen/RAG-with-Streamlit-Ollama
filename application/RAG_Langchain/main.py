2d8f25e7dd214fa6af0b36e73c705f4aafdf1f80
Andrei <test@test.mail>
2024-08-09 16:57:41 +0300

    RAG Langchain

--- application/RAG_Langchain/main.py
+++ application/RAG_Langchain/main.py
@@ -1,4 +1,8 @@
 from langchain_community.document_loaders import WebBaseLoader
+import os
+
+os.environ['USER_AGENT'] = 'Chrome/127.0.0.0 Safari/537.36'
+
 
 loader = WebBaseLoader("https://ru.wikipedia.org/wiki/%D0%93%D1%80%D0%B0%D0%B2%D0%B8%D1%86%D0%B0%D0%BF%D0%BF%D0%B0")
 data = loader.load()
