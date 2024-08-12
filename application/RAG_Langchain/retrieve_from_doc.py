e41f7ce3d8638cd0997d9e75d3fcc2ec9e6ef338
Andrei <test@test.mail>
2024-08-12 17:06:38 +0300

    RAG PDF Retrieval

--- application/RAG_Langchain/retrieve_from_doc.py
+++ application/RAG_Langchain/retrieve_from_doc.py
@@ -2,8 +2,14 @@ from langchain_experimental.text_splitter import SemanticChunker
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain_community.document_loaders import PDFPlumberLoader
 
+# Load data
 loader = PDFPlumberLoader("test_doc.pdf")
 docs = loader.load()
+
+
+# Split data
+text_splitter = SemanticChunker(HuggingFaceEmbeddings())
+documents = text_splitter.split_documents(docs)
 
 
 
