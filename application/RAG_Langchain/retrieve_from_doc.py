73c0623de1f7fec9ee722476e74f94abe16f8800
Andrei <test@test.mail>
2024-08-12 17:34:38 +0300

    RAG PDF Retrieval

--- application/RAG_Langchain/retrieve_from_doc.py
+++ application/RAG_Langchain/retrieve_from_doc.py
@@ -1,8 +1,6 @@
 from langchain_community.document_loaders import PDFPlumberLoader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain_community.vectorstores import Chroma
-from langchain_community import embeddings
-from langchain_community.embeddings import OllamaEmbeddings
+
 
 
 # Load data
@@ -14,11 +12,5 @@ docs = loader.load()
 text_splitter = CharacterTextSplitter.from_tiktoken_encoder(chunk_size=7500, chunk_overlap=100)
 doc_splits = text_splitter.split_documents(docs)
 
-# Create a vector store using Chroma DB, our chunked data from the URLs, and the nomic-embed-text embedding model
-vectorstore = Chroma.from_documents(
-    documents=doc_splits,
-    collection_name="rag-chroma",
-    embedding=embeddings.ollama.OllamaEmbeddings(model='mxbai-embed-large'),
-    )
-retriever = vectorstore.as_retriever()
+
 
