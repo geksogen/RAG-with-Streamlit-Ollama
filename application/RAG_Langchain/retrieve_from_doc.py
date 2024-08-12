fca8a980c5fa76d263187e89e693006ccf99340b
Andrei <test@test.mail>
2024-08-12 17:03:01 +0300

    RAG PDF Retrieval

--- application/RAG_Langchain/retrieve_from_doc.py
+++ application/RAG_Langchain/retrieve_from_doc.py
@@ -5,6 +5,5 @@ from langchain_community.document_loaders import PDFPlumberLoader
 loader = PDFPlumberLoader("test_doc.pdf")
 docs = loader.load()
 
-# Check the number of pages
-print("Number of pages in the PDF:",len(docs))
+
 
