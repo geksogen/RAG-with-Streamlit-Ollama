9e8ef0e3eaa1dd92d83b7eef0e44289beb5d7ccd
Andrei <test@test.mail>
2024-08-13 11:42:34 +0300

    RAG

---
+++ application/RAG_Langchain/RAG_langchain.py
@@ -0,0 +1,18 @@
+from langchain_community.vectorstores.chroma import Chroma
+from langchain_community.chat_models.ollama import ChatOllama
+from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
+from langchain.schema.output_parser import StrOutputParser
+from langchain_community.document_loaders.pdf import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.schema.runnable import RunnablePassthrough
+from langchain.prompts import PromptTemplate
+from langchain_community.vectorstores.utils import filter_complex_metadata
+
+model = ChatOllama(model="orca-mini:3b")
+
+
+docs = PyPDFLoader(file_path="test_doc.pdf").load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=100)
+chunks = text_splitter.split_documents(docs)
+chunks = filter_complex_metadata(chunks)
