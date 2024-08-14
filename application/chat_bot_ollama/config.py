c666bbd86dc313599787df51cb0a0e869843d78c
Andrei <test@test.mail>
2024-08-14 10:52:21 +0300

    tinyllama add

--- application/chat_bot_ollama/config.py
+++ application/chat_bot_ollama/config.py
@@ -1,7 +1,7 @@
 class Config:
     PAGE_TITLE = "Chatbot на локальной LLM модельки"
 
-    OLLAMA_MODELS = ('owl/t-lite', 'orca-mini:3b')
+    OLLAMA_MODELS = ('owl/t-lite', 'orca-mini:3b', "tinyllama")
 
     SYSTEM_PROMPT = f"""You are a helpful chatbot that has access to the following
                     open-source models {OLLAMA_MODELS}.
